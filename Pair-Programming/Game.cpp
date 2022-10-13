fffa069e995cec316289fe3daf7ed065f155fd82
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2022-10-13 11:17:06 -0700

    Added click functionality

--- Pair-Programming/Game.cpp
+++ Pair-Programming/Game.cpp
@@ -1,10 +1,13 @@
 #include "Game.h"
+using namespace std;
 using namespace gm;
 using namespace sf;
 
 class GameObject {
 private:
 	Shape* shape;
+	ParticleEffect* pEffect;
+
 public:
 	GameObject(const Vector2f& position, float size) {
 		shape = new CircleShape(size);
@@ -23,6 +26,11 @@ public:
 		window.clear();
 		window.display();
 	}
+
+	ParticleEffect(const Vector2f& position) {
+		pEffect = new ParticleEffect;
+		pEffect->setPosition(position)
+	}
 };
 
 Game::Game() {
@@ -40,11 +48,14 @@ void Game::handleInput(sf::RenderWindow& window) {
 		if (event.type == sf::Event::Closed) {
 			window.close();
 		}
+
+		if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+			cout << "Here goes the particle effect" << endl;
+		}
 	}
 }
 
 void Game::update(sf::RenderWindow& window) {
-	std::cout << "update" << std::endl;
 }
 
 void Game::render(sf::RenderWindow& window) {
